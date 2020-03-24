ae305d9af696b8b6a386e131d13734171f06292a
rishi-shah12 <60271537+rishi-shah12@users.noreply.github.com>
2020-03-24 15:31:39 -0400

    Update calculate.sh

--- calculate.sh
+++ calculate.sh
@@ -8,8 +8,3 @@ cd $1
 
 #find all files, return their md5sums to std out
 find . -name "*.vcf.gz" -exec md5sum {} +
-count=`ls -1 *.maf.gz | wc -l`
-if [ $count !=0 ]; then
-   zcat *.maf.gz | cut -f1 | sort | uniq | wc -l
-   zcat *.maf.gz | cut -f16 | sort | uniq | wc -l
-fi
